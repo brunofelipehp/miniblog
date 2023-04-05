bad797dbc6c6a7ab460c173ff0da6488d9a88bca
Bruno Felipe <brunofelipehp8@gmail.com>
2023-04-05 16:56:15 -0300

    navbar and footer

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,9 @@
 import styles from "./About.module.css";
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,9 @@
 import styles from "./Home.module.css";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 };
